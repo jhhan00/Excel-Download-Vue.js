be26bda3b6de721c2ce0f89046c3aed1c138143e
jhhan00 <48585234+jhhan00@users.noreply.github.com>
2021-01-30 22:45:57 +0900

    commit - excel download3

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,9 @@
     <div>
       <button @click="makeExcelFile2">Excel2</button>
     </div>
+    <div>
+      <button @click="makeExcelFile3">Excel3</button>
+    </div>
   </div>
 </template>
 
@@ -56,7 +59,9 @@ export default {
     },
     makeExcelFile () {
       console.log("Excel!")
-      axios.post(process.env.VUE_APP_API_ENDPOINT + "/excel", this.data1, { responseType: 'arraybuffer' })
+      axios.post(process.env.VUE_APP_API_ENDPOINT + "/excel", this.data1, {
+        responseType: 'arraybuffer'
+      })
           .then(result => {
             console.log(result)
             console.log(result.headers["content-type"])
@@ -73,18 +78,37 @@ export default {
     },
     makeExcelFile2 () {
       console.log("Excel-2!")
-      axios.post(process.env.VUE_APP_API_ENDPOINT + "/excel2", this.data1, { responseType: 'arraybuffer' })
-      .then(result => {
-        console.log(result)
-        const url = window.URL.createObjectURL(new Blob([result.data], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" }))
-        const link = document.createElement("a")
-        link.href = url
-        link.download = "example.xlsx"
-        link.click()
-        window.URL.revokeObjectURL(url)
-      }).catch(er => {
-        console.log(er)
+      axios.post(process.env.VUE_APP_API_ENDPOINT + "/excel2", this.data1, {
+        responseType: 'arraybuffer'
       })
+          .then(result => {
+            console.log(result)
+            const url = window.URL.createObjectURL(new Blob([result.data], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" }))
+            const link = document.createElement("a")
+            link.href = url
+            link.download = "example.xlsx"
+            link.click()
+            window.URL.revokeObjectURL(url)
+          }).catch(er => {
+            console.log(er)
+          })
+    },
+    makeExcelFile3 () {
+      console.log("Excel-3!")
+      axios.post(process.env.VUE_APP_API_ENDPOINT + "/excel3", this.data1, {
+        responseType: 'arraybuffer'
+      })
+          .then(result => {
+            console.log(result)
+            const url = window.URL.createObjectURL(new Blob([result.data], { type: result.headers["content-type"] }))
+            const link = document.createElement("a")
+            link.href = url
+            link.download = "example.xlsx"
+            link.click()
+            window.URL.revokeObjectURL(url)
+          }).catch(er => {
+            console.log(er)
+          })
     }
   }
 }
